39278b315f4ad90eb87525bc96ddc5887c489a94
Anders Norman <anorman@berkeley.edu>
2013-11-28 14:47:09 +0100

    Minor update

--- Scripts/class_from_blast.pl
+++ Scripts/class_from_blast.pl
@@ -8,14 +8,18 @@ use Getopt::Long;
 
 use Data::Dumper;
 
-my ($blast_file, $names_file, $cls_file, $subseq_index, $sort_by_size );
+my ($blast_file, $names_file, $cls_file, $subseq_index, $sort_by_size, $min_class_size );
+
+$min_class_size = 0;
 
 &GetOptions(
 	'names|n=s'    => \$names_file,
 	'blast|b=s'    => \$blast_file,
 	'cls|c=s'      => \$cls_file,
-	'sort_by_size' => \$sort_by_size	
+	'sort_by_size' => \$sort_by_size,
+	'min_size=i'   => \$min_class_size	
 );
+
 my $names   = Anorman::ESOM::File::Names->new( $names_file );
 
 $names->load;
@@ -30,6 +34,7 @@ while (defined (my $line = <$FH>)) {
 	chomp $line;
 	my ($query,$hit) = split ("\t", $line);
 	$BLAST_RESULT{ $query } = $hit;
+	$CLASS_NAME{ $hit } ||= 0;
 	$CLASS_NAME{ $hit } += scalar @{ $subseq_index->{ $query } }
 		if exists $subseq_index->{ $query };
 }
@@ -40,10 +45,16 @@ my $cls     = Anorman::ESOM::File::Cls->new( $cls_file );
 
 # Make new classes according to blast hits
 my $classes     = Anorman::ESOM::ClassTable->new;
-my @class_names = $sort_by_size ? sort { $CLASS_NAME{ $b } <=> $CLASS_NAME{ $a } } keys %CLASS_NAME :
-		  sort keys %CLASS_NAME;
-my $class_index = 0;
 
+# Sort and filter classes
+my @class_names = grep { $CLASS_NAME{ $_ } >= $min_class_size } $sort_by_size ? 
+			sort { $CLASS_NAME{ $b } <=> $CLASS_NAME{ $a } ||
+			       $a cmp $b 
+			     } keys %CLASS_NAME :
+
+			sort keys %CLASS_NAME;
+
+my $class_index = 0;
 foreach my $class_name(@class_names) {
 	$classes->add( ++$class_index, $class_name );
 }
@@ -58,6 +69,9 @@ my %tmp_cls = ();
 while (my($seq_name, $members) = each %{ $subseq_index }) {
 	if (exists $BLAST_RESULT{ $seq_name }) {
 		my $class       = $classes->get_by_name( $BLAST_RESULT{ $seq_name } );
+
+		next unless defined $class;
+
 		my $class_index = $class->index;
 
 		foreach my $index(@{ $members }) {
